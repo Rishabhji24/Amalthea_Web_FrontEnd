507ef37f2732bce943158c6b75b8680401ccfbb2
Reuben27 <reuben.sd@iitgn.ac.in>
2020-09-23 23:23:52 +0530

    Updates

--- FrontEnd_3/static/js/events.js
+++ FrontEnd_3/static/js/events.js
@@ -71,3 +71,19 @@ function carousel() {
     setTimeout(carousel, 5000); // Change image every 2 seconds
 }
 */
+
+//Navbar
+var i = 0;
+document.getElementById("img-btn").onclick = function() {
+    i++;
+    if (i % 2 != 0) {
+        var nav = document.getElementById("hor-nav");
+        nav.style.width = "100%";
+        nav.style.height = "80%";
+    } 
+    else {
+        var nav = document.getElementById("hor-nav");
+        nav.style.width = "80px";
+        nav.style.height = "60px";
+    }
+}
