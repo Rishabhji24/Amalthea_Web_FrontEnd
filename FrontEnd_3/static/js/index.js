2d2fc10401da97e1c9cfa07f6f1bf24502bfa34b
Reuben27 <reuben.sd@iitgn.ac.in>
2020-09-24 20:51:51 +0530

    Updates

---
+++ FrontEnd_3/static/js/index.js
@@ -0,0 +1,15 @@
+//Navbar
+var i = 0;
+document.getElementById("img-btn").onclick = function() {
+    i++;
+    if (i % 2 != 0) {
+        var nav = document.getElementById("hor-nav");
+        nav.style.width = "100%";
+        nav.style.height = "80%";
+    }
+    else {
+        var nav = document.getElementById("hor-nav");
+        nav.style.width = "80px";
+        nav.style.height = "60px";
+    }
+}
