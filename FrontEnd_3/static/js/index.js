20956b37a6f3dd670e8d67118a839a465329f8b9
Gaurav Viramgami <58519896+Gaurav7214@users.noreply.github.com>
2020-09-25 13:18:17 +0530

    Navbar Transition(HomePage)

--- FrontEnd_3/static/js/index.js
+++ FrontEnd_3/static/js/index.js
@@ -5,7 +5,10 @@ document.getElementById("img-btn").onclick = function() {
     if (i % 2 != 0) {
         var nav = document.getElementById("hor-nav");
         nav.style.width = "100%";
-        nav.style.height = "80%";
+        var width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        if (width < 900) {
+          nav.style.height = "80%";
+        }
     }
     else {
         var nav = document.getElementById("hor-nav");
@@ -13,3 +16,32 @@ document.getElementById("img-btn").onclick = function() {
         nav.style.height = "60px";
     }
 }
+
+var myNav = document.getElementById('header');
+window.onscroll = function () {
+    "use strict";
+    if (document.body.scrollTop >= 600 || document.documentElement.scrollTop >= 600) {
+      $('.img-col input').attr('src','static/images/logo_white.svg');
+        myNav.classList.add("scroll");
+        myNav.classList.remove("no-scroll");
+    }
+    else {
+      $('.img-col input').attr('src','static/images/logo.svg');
+        myNav.classList.add("no-scroll");
+        myNav.classList.remove("scroll");
+    }
+};
+
+// $(window).scroll(function(){
+//   var scroll = $(window).scrollTop();
+//   if (scroll > 600) {
+//     var nav = document.getElementById("header");
+//     nav.classList.remove("no-scroll");
+//     nav.classList.add("scroll");
+//   }
+//   else{
+//     var nav = document.getElementById("header");
+//     nav.classList.remove("scroll");
+//     nav.classList.add("no-scroll");
+//   }
+// })
