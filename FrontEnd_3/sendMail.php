bb64f476f21fc11561f070b6839d516cc641bf10
Gaurav Viramgami <58519896+Gaurav7214@users.noreply.github.com>
2020-10-15 00:55:01 +0530

    check

--- FrontEnd_3/sendMail.php
+++ FrontEnd_3/sendMail.php
@@ -1,14 +1,25 @@
 <?php
-  $name = $_POST['name'];
-  $email = $_POST['email'];
 
-  $email_from = 'amalthea@iitgn.ac.in';
-  $subject = 'You registered for Webinar | Amalthea \'20';
-  $message = "Hello ".$name.",<br><br> Greetings from Amalthea '20<br><br>Thanks for registering for Webinar. We will send you the joining link soon.<br><br>Regards<br>Webinars Team Amalthea";
-  $headers = "MIME-Version: 1.0" . "\r\n";
-  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-  $headers .= 'From: noreply @ amalthea.iitgn.ac.in';
-  $to = $email;
-  mail($to,$subject,$message,$headers);
-  header("Location: webinar_form.html");
+    if(isset($_GET['name'])&&isset($_GET['institute'])&&isset($_GET['professional_status'])&&isset($_GET['email'])&&!empty($_GET['email'])&&!empty($_GET['name'])){
+        $name = htmlentities($_GET['name']);
+        $email = htmlentities($_GET['email']);
+        $institute = htmlentities($_GET['institute']);
+        $professional_status = htmlentities($_GET['professional_status']);
+        // if(empty($phone)){
+        //     $toDisplay = "";
+        // } else {
+        //     $toDisplay = "<br>Phone : <b>".$phone."</b>";
+        // }
+        $to_email = $email;
+        $subject = 'You registered for Webinar | Amalthea \'20';
+        $message = "Hello ".$name.",<br><br> Greetings from Amalthea '20<br><br>Thanks for registering for the webinar. We will send you the joining link soon. <br><br>Regards<br>Webinars Team Amalthea";
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $headers .= 'From: noreply @ amalthea.iitgn.ac.in';
+        if(mail($to_email,$subject,$message,$headers)) {
+            echo "done";
+        }
+    } else {
+        header("Location:webinar_form.html");
+    }
 ?>
